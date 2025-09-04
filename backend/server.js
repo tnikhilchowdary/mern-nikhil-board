09f1dbedbd02c819927bd125915702f898a97053
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-09-04 14:45:38 -0500

    Basic Setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
-const express = require("express");
-
+const PORT = 5000;
 const app =  express();
 
-app.listen(5001, () => {
-    console.log("Server running on PORT");
+app.get("/api/notes", (req, res) => {
+    res.send("You got 5 Notes, welcome to the Backend");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`);
 });
 
