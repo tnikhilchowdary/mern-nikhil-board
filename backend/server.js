04405d35cd9406553d2683b9ed336e31728eb3d8
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-09-08 11:16:39 -0500

    Basic Nodejs and express setup

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,20 @@ const PORT = 5000;
 const app =  express();
 
 app.get("/api/notes", (req, res) => {
-    res.send("You got 5 Notes, welcome to the Backend");
+    res.status(200).send("You got 20 notes");
 });
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"Note Created Succesfully"});
+})
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"Note Updated Succesfully"});
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"Note Deleted Succesfully!"});
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
