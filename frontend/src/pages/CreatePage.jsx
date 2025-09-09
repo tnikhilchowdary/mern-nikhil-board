2fcc29b9093c0f5a6111efdc3298682e8ff0401f
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-09-09 16:15:38 -0500

    Frontend Setup

---
+++ frontend/src/pages/CreatePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CreatePage(){
+    return(
+        <h1>Welcome to the Create Page</h1>
+    )
+}
+
+export default CreatePage;
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function NoteDetailPage(){
+    return(
+        <div>
+            <h1>Welcome to the Note Detail page</h1>
+        </div>
+    )
+}
+
+export default NoteDetailPage
